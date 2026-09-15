467775db3f497dc0d794e6f7e37715e16c3d2764
SuchakreeS <poomfail2013@gmail.com>
2026-09-15 09:12:35 +0700

    feat: add get steam id and profile pic functions

--- backend/steam_api.py
+++ backend/steam_api.py
@@ -16,3 +16,31 @@ def get_owned_games(steamid: str, api_key:str) -> list[dict] | None :
 
     games = data.get("response", {}).get("games")
     return games
+
+def _get_player_summary(steamid, api_key):
+    url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/"
+    params = {
+        "key" : api_key,
+        "steamids" : steamid
+    }
+    res = requests.get(url, params=params)
+    data = res.json()
+    players = data.get("response", {}).get("players", [])
+
+    if not players:
+        return None
+
+    return players[0]
+
+def get_steam_name(steamid, api_key):
+    player = _get_player_summary(steamid, api_key)
+    if player is None:
+        return None
+    return player.get("personaname")
+
+def get_profile_pic(steamid, apikey):
+    player = _get_player_summary(steamid, apikey)
+    if player is None:
+        return None
+    return player.get("avatarfull")
+
