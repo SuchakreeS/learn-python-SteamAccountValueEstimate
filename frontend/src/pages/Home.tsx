64d74361e00c312e912fdf6105cd8ccb8c4d9459
SuchakreeS <poomfail2013@gmail.com>
2026-09-15 13:35:39 +0700

    feat: add Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,17 @@
-import { useGamesStore } from "../store/useGamesStore";
-import { priceTotals } from "../utils/priceTotals";
+import ConfettiBg from "../components/ConfettiBg";
+import ProfileHeader from "../components/ProfileHeader";
+import StatCard from "../components/StatCard";
+import BottomNav from "../components/BottomNav";
 
 export default function Home() {
-    const { games, gameCount } = useGamesStore();
-    const { totalMin, totalMax } = priceTotals(games);
-
     return (
-        <div>
-            <h1>Your Library ({gameCount} games)</h1>
-            <h2>Estimated Value: ${totalMin.toFixed(2)} - ${totalMax.toFixed(2)}</h2>
-            <ul>
-                {games.map((game) => (
-                    <li key={game.appId}>
-                        {game.name} - min: {game.min ?? "N/A"}, max: {game.max ?? "N/A"}
-                    </li>
-                ))}
-            </ul>
+        <div className="relative min-h-screen overflow-hidden bg-bg px-6 pb-32 pt-10">
+            <ConfettiBg />
+            <div className="relative z-10 mx-auto flex max-w-3xl flex-col gap-10">
+                <ProfileHeader />
+                <StatCard />
+            </div>
+            <BottomNav />
         </div>
     );
 }
